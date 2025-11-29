6c7dce7bdda8af361860579d83285521d7a18388
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-11-29 15:00:33 +0530

    first commit

---
+++ src/main/java/com/binoj/laptop_price_server/LaptopPriceServerApplication.java
@@ -0,0 +1,13 @@
+package com.binoj.laptop_price_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopPriceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopPriceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binoj/laptop_price_server/LaptopPriceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binoj.laptop_price_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopPriceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
